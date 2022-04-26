7efc23a2bc7236b3945a20e5e2ffa1de2459185a
rg819 <96975314+RatnajiGuptha@users.noreply.github.com>
2022-04-26 11:15:15 +0530

    11

--- DoctorDAo.java
+++ DoctorDAo.java
@@ -14,7 +14,7 @@ public class DoctorDAO {
 		doctorList.add(new Doctor(1,"Harinii","MBBS",4.2,"Orthologist",750.00));
 		doctorList.add(new Doctor(2,"Nithin","MBBS",2.0,"Gynecologist",1500.00));
     		doctorList.add(new Doctor(//fill the values as per question));
-    		doctorList.add(new Doctor(//fill the values as per question);
+    		doctorList.add(new Doctor(//fill the values as per question));
 	  }	
 
 	public Boolean save(Doctor doctor) {
